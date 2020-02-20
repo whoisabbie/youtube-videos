31bb64f6e37f4e3aaae3c440f15bd6fd3f1f6e56
whoisabbie <44428844+whoisabbie@users.noreply.github.com>
2020-02-20 10:46:52 +0530

    Final commit.

---
+++ src/components/VideoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import VideoItem from './VideoItem'
+
+const VideoList = ({ videos, onVideoSelect }) => {
+    //props.videos from youtube videos
+    const renderedList = videos.map((video) => {
+        return <VideoItem key={video.id.videoId} video={video} onVideoSelect={onVideoSelect} />
+    });
+    return (
+        <div className="ui relaxed divided list">{renderedList}</div>
+    );
+};
+
+export default VideoList;
